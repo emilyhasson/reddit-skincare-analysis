a0c64528194a5d4fba559812c7905f1ba20b2484
Emily Hasson <ehasson2@illinois.edu>
2024-01-03 17:23:48 -0600

    adjusting dataframes

--- src/Scraping/scraper.py
+++ src/Scraping/scraper.py
@@ -18,19 +18,18 @@ for submission in reddit.subreddit('SkincareAddiction').hot(limit=5):
     headlines.add((submission.title, "https://www.reddit.com" + submission.permalink))
 
 # Convert set of tuples to Pandas DataFrame
-df = pd.DataFrame(headlines, columns=['Title', 'URL'])
+headlines_df = pd.DataFrame(headlines, columns=['Title', 'URL'])
 
 # Display the DataFrame
-print(df)
+print(headlines_df)
 
 
 
 
 # Getting page content
 
-urls = df['URL']
-print(urls[2])
-url = urls[2]
+urls = headlines_df['URL']
+comments_df = pd.DataFrame(columns=["URL", "body"])
 
 for url in urls:
     submission = reddit.submission(url=url)
@@ -41,5 +40,33 @@ for url in urls:
         if (top_level_comment.body == '[removed]') | (top_level_comment.body == '[deleted]'):
             continue
         posts.append(top_level_comment.body)
-    posts = pd.DataFrame(posts,columns=["body"])
-    print(posts)
+    posts_df = pd.DataFrame(posts,columns=["body"])
+    posts_df["URL"] = url  # Add a new column with the current URL
+    comments_df = pd.concat([comments_df, posts_df], ignore_index=True)
+    # print(posts)
+
+print(comments_df)
+
+# AWS
+
+# from pyspark.sql import SparkSession
+
+# # Assuming you have Pandas DataFrames: pandas_article_df and pandas_comment_df
+
+# # Create a Spark session
+# spark = SparkSession.builder \
+#     .appName("Pandas to PySpark") \
+#     .getOrCreate()
+
+# # Convert Pandas DataFrames to PySpark DataFrames
+# spark_article_df = spark.createDataFrame(pandas_article_df)
+# spark_comment_df = spark.createDataFrame(pandas_comment_df)
+
+# # Show the PySpark DataFrames
+# spark_article_df.show()
+# spark_comment_df.show()
+
+# # Perform any PySpark operations on the DataFrames if needed
+
+# # Stop the Spark session
+# spark.stop()
